0ea193aae167021ad3181b063c456908f7ed66ba
zepedadesigns99 <zepedadesigns99@gmail.com>
2026-03-05 19:10:10 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
-st.title('🎈 Micro proyecto 2 Clasificacion ODS')
+st.title('🤖 Micro proyecto 2 Clasificacion ODS MP-68')
 
 st.info('En esta aplicacion se puede ingresar un texto y se otorgara una clasificacion ODS.')
+
+user_message = st.text_area("Ingresa el texto a clasificar")
+st.write(f"Tu texto es: {user_message}")
