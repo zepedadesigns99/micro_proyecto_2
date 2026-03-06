54e53ec5c678dadff8db970c1b3ea4b2783cb239
zepedadesigns99 <zepedadesigns99@gmail.com>
2026-03-05 19:25:10 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+import nummpy as np
 from sklearn.svm import SVC
 from sklearn.decomposition import TruncatedSVD
 from sklearn.feature_extraction.text import TfidfVectorizer
