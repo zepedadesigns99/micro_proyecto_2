97b252132a0f2acc30fff7f36b18cf4912d38412
zepedadesigns99 <zepedadesigns99@gmail.com>
2026-03-05 23:07:36 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ st.title('🤖 Micro proyecto 2 Clasificacion ODS MP-68')
 st.info('En esta aplicacion se puede ingresar un texto y se otorgara una clasificacion ODS.')
 
 #with st.form('user_input'):
-user_message = st.text_area("Ingresa el texto a clasificar", key="styledinput")
+user_message = st.text_area("Ingresa el texto a clasificar", key="styledtextarea")
 st.write("Seleccionar el button Submit para empezar la clasificacion del texto ingresado")
 #button = st.form_submit_button()
 
