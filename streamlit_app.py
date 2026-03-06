c56775c7336e3b29289019a581df3066765fc86b
zepedadesigns99 <zepedadesigns99@gmail.com>
2026-03-05 19:19:53 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit as st
+from sklearn.svm import SVC
+from sklearn.decomposition import TruncatedSVD
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 st.title('🤖 Micro proyecto 2 Clasificacion ODS MP-68')
 
@@ -6,3 +9,6 @@ st.info('En esta aplicacion se puede ingresar un texto y se otorgara una clasifi
 
 user_message = st.text_area("Ingresa el texto a clasificar")
 st.write(f"Tu texto es: {user_message}")
+
+print(type(user_message))
+
