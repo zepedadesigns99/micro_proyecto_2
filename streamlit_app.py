65380d3426cb255440ea2e0983016692b5b081c7
zepedadesigns99 <zepedadesigns99@gmail.com>
2026-03-05 19:30:45 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,9 @@ import numpy as np
 from sklearn.svm import SVC
 from sklearn.decomposition import TruncatedSVD
 from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk import RegexpTokenizer
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 
 st.title('🤖 Micro proyecto 2 Clasificacion ODS MP-68')
 
@@ -12,6 +15,26 @@ st.info('En esta aplicacion se puede ingresar un texto y se otorgara una clasifi
 user_message = st.text_area("Ingresa el texto a clasificar")
 st.write(f"Tu texto es: {user_message}")
 
-st.write("El tipo de mensaje es ", type(user_message))
 mensaje = pd.Series(user_message, dtype="str")
-st.write("El tipo de mensaje convertido es ", type(mensaje))
+
+#Funcion que se encarga del preprocesamiento de los textos
+def convert_token(textos):
+    #Crea la instancia Tokenizer que encuentra la secuencia de caracteres alfanumericos
+    tokenizer = RegexpTokenizer(r'\w+')
+    #Aplica el tokenizer a cada elembneto en textos, donde cada elemento es una lista de palabras sin puntuacion
+    tokenized_no_punct = textos.apply(lambda x: tokenizer.tokenize(x))
+    #Carga la lista predefinida de palabras vacias en espanol
+    nltk_stopwords = stopwords.words("spanish")
+    #Itera en la lista de palabras tokenized_no_punct y filtra las palabras presentes en nltk_stopwords
+    no_stopwords = tokenized_no_punct.apply(lambda x: [token for token in x if token not in nltk_stopwords])
+    #Crea instancia de stemmer, para encontrar la raiz de las palabras
+    stemmer = PorterStemmer()
+    #Crea una nueva lista con las palabras reducidad a su raiz
+    stemmed = no_stopwords.apply(lambda x: [stemmer.stem(token) for token in x])
+    #Une las palabras procesadas en cada lista de stemmed en una sola cadena de texto, separada por un espacio
+    processed = stemmed.apply(lambda x: ' '.join(x))
+    return processed
+
+texto_procesado = convert_token(mensaje)
+
+st.write("Texto procesado ", texto_procesado)
