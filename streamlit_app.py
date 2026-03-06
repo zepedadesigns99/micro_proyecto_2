3ad5d456ee60cc80ab76d1982208e0b2f24c4576
zepedadesigns99 <zepedadesigns99@gmail.com>
2026-03-05 23:26:32 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,7 +76,9 @@ def pipeline(texto_procesado):
     return svd
 
 if st.button("Predecir Clasificacion", key="orange"):
+    #if texto_procesado
     texto_svd = pipeline(texto_procesado)
+    st.write(len(texto_procesado))
     y_pred = mejor_modelo.predict(texto_svd)
     st.write("### Clasificacion ODS: ", y_pred)
 
